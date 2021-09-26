af3712d30ab455bb486c4aad0e79d40dd9446a53
Alexander <61922116+Lazovski1991@users.noreply.github.com>
2021-09-26 23:52:06 +0300

    Update AddressRepository.java

--- src/main/java/com/yakut/spring/repository/AddressRepository.java
+++ src/main/java/com/yakut/spring/repository/AddressRepository.java
@@ -3,6 +3,6 @@ package com.yakut.spring.repository;
 import com.yakut.spring.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//todo нахуя оно?
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
